7d2c892f0c36deb477c8702894e7d6f2d4ca067a
Minttu Gomez <minttu.jamsa@helsinki.fi>
2023-11-20 20:44:04 +0800

    Project reader update

---
+++ viikko2/project-reader/src/project_reader.py
@@ -0,0 +1,16 @@
+from urllib import request
+from project import Project
+import toml
+
+
+class ProjectReader:
+    def __init__(self, url):
+        self._url = url
+
+    def get_project(self):
+        # tiedoston merkkijonomuotoinen sisältö
+        content = request.urlopen(self._url).read().decode("utf-8")
+        dict = toml.loads(content)
+
+        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
+        return Project(dict['tool']['poetry']['name'], dict['tool']['poetry']['description'], dict['tool']['poetry']['license'], dict['tool']['poetry']['authors'], dict['tool']['poetry']['dependencies'], dict['tool']['poetry']['group']['dev']['dependencies'])
